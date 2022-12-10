a4905ff3bbe9c0eb11187beab7dbf1eb25f4f738
Klvst3r <klvst3r@gmail.com>
2022-12-10 16:05:08 -0600

    Crando libros en la BD con la API

--- pages/libros/crear.js
+++ pages/libros/crear.js
@@ -2,19 +2,32 @@ import Link from "next/link";
 import {useState} from "react";
 
 const BookCreate = () => {
-    const [bookName, setBookName] = useState('')
-    function handleSubmit(e){
+    const [bookTitle, setBookTitle] = useState('')
+    async function handleSubmit(e){
         e.preventDefault()
-        console.log(bookName)
+        /*console.log(bookName)*/
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/books`, {
+            method: 'POST',
+            headers: {
+              accept: 'application/json',
+              'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: bookTitle
+            })
+        })
+
+        console.log(res)
     }
 
     return (
         <>
         <h1>BookCreate</h1>
-            <p>{bookName}</p>
+            <p>{bookTitle}</p>
             <form onSubmit={handleSubmit}>
                 <input
-                    onChange={(e) => setBookName(e.target.value)}
+                    onChange={(e) => setBookTitle(e.target.value)}
+                    value={bookTitle}
                     type="text"
                 />
                 <button>Enviar</button>
