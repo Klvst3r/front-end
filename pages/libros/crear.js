cc2903bd47c2d30d2979fe7e46b8efa9c87c6176
Klvst3r <klvst3r@gmail.com>
2022-12-10 13:42:25 -0600

    Impresion en consola del valor desde el formulario

--- pages/libros/crear.js
+++ pages/libros/crear.js
@@ -1,10 +1,25 @@
-import Head from "next/head";
 import Link from "next/link";
+import {useState} from "react";
 
 const BookCreate = () => {
+    const [bookName, setBookName] = useState('')
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(bookName)
+    }
+
     return (
         <>
         <h1>BookCreate</h1>
+            <p>{bookName}</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    onChange={(e) => setBookName(e.target.value)}
+                    type="text"
+                />
+                <button>Enviar</button>
+            </form>
+            <br/>
         <Link href="/libros">Book List</Link>
         </>
     );
